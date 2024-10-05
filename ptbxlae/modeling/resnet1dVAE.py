999db1e20f00d4733c53449cfb96643e6926b4c9
isears <isaac.j.sears@gmail.com>
2024-10-04 20:47:05 -0400

    Implement resnet decoder but still need to solve padding issues

--- ptbxlae/modeling/resnet1dVAE.py
+++ ptbxlae/modeling/resnet1dVAE.py
@@ -13,6 +13,7 @@ from torch.nn import (
     Sequential,
     Flatten,
     Dropout,
+    Unflatten,
 )
 
 from torchinfo import summary
@@ -20,46 +21,69 @@ import lightning as L
 
 
 class ResidualBlock(torch.nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, kernel_size: int):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int,
+        decoder: bool = False,
+    ):
         super(ResidualBlock, self).__init__()
 
-        self.conv1 = Conv1d(
+        same_padding = (kernel_size - 1) // 2
+
+        if decoder:
+            self.conv_layer_builder = (
+                lambda in_channels, out_channels, kernel_size: ConvTranspose1d(
+                    in_channels=in_channels,
+                    out_channels=out_channels,
+                    kernel_size=kernel_size,
+                    padding=same_padding,
+                    bias=False,
+                )
+            )
+        else:
+            self.conv_layer_builder = (
+                lambda in_channels, out_channels, kernel_size: Conv1d(
+                    in_channels=in_channels,
+                    out_channels=out_channels,
+                    kernel_size=kernel_size,
+                    padding=same_padding,
+                    bias=False,
+                )
+            )
+
+        self.conv1 = self.conv_layer_builder(
             in_channels=in_channels,
             out_channels=out_channels,
             kernel_size=kernel_size,
-            padding="same",
-            bias=False,
         )
 
         self.bn1 = BatchNorm1d(out_channels)
         self.activation = ReLU()
 
-        self.conv2 = Conv1d(
+        self.conv2 = self.conv_layer_builder(
             in_channels=out_channels,
             out_channels=out_channels,
             kernel_size=kernel_size,
-            padding="same",
-            bias=False,
         )
 
         self.bn2 = BatchNorm1d(out_channels)
 
-        self.downsample = None
+        self.down_or_up_sample = None
 
         if in_channels != out_channels:
-            self.downsample = Sequential(
-                Conv1d(
+            self.down_or_up_sample = Sequential(
+                self.conv_layer_builder(
                     in_channels=in_channels,
                     out_channels=out_channels,
                     kernel_size=kernel_size,
-                    padding="same",
-                    bias=False,
                 ),
                 BatchNorm1d(out_channels),
             )
 
     def forward(self, x):
-        identity = self.downsample(x) if self.downsample else x
+        identity = self.down_or_up_sample(x) if self.down_or_up_sample else x
 
         x = self.conv1(x)
         x = self.bn1(x)
@@ -75,20 +99,22 @@ class Resnet1dEncoder(torch.nn.Module):
 
     def __init__(
         self,
-        depth: int,
+        # depth: int, TODO
         kernel_size: int,
         dropout: float,
         latent_dim: int,
+        channels_in: int = 12,
+        seq_len: int = 5000,
     ):
         super(Resnet1dEncoder, self).__init__()
 
-        channels = 12
+        same_padding = (kernel_size - 1) // 2
 
         self.conv = Conv1d(
-            in_channels=channels,
+            in_channels=channels_in,
             out_channels=24,
             kernel_size=kernel_size,
-            padding="same",
+            padding=same_padding,
             bias=False,
         )
         self.bn = BatchNorm1d(24)
@@ -122,9 +148,11 @@ class Resnet1dEncoder(torch.nn.Module):
             AvgPool1d(kernel_size=(kernel_size - 1), stride=(kernel_size - 1)),
         )
 
+        last_layer_size = 192 * (seq_len // ((kernel_size - 1) ** 4))
+
         self.flatten = Flatten(1)
         self.dropout = Dropout(p=dropout)
-        self.linear = Linear(in_features=3648, out_features=latent_dim)
+        self.linear = Linear(in_features=last_layer_size, out_features=latent_dim)
 
     def forward(self, x):
         x = self.conv(x)
@@ -143,12 +171,150 @@ class Resnet1dEncoder(torch.nn.Module):
         return x
 
 
+class Resnet1dDecoder(torch.nn.Module):
+
+    def __init__(
+        self,
+        # depth: int,
+        kernel_size: int,
+        dropout: float,
+        latent_dim: int,
+        channels_out: int = 12,
+        seq_len: int = 5000,
+    ):
+        super(Resnet1dDecoder, self).__init__()
+
+        first_layer_size = 192 * (seq_len // ((kernel_size - 1) ** 4))
+        same_padding = (kernel_size - 1) // 2
+
+        self.linear = Linear(in_features=latent_dim, out_features=first_layer_size)
+        self.dropout = Dropout(dropout)
+        self.unflatten = Unflatten(dim=1, unflattened_size=(192, -1))
+
+        self.layer1 = Sequential(
+            ResidualBlock(192, 96, kernel_size, decoder=True),
+            ResidualBlock(96, 96, kernel_size, decoder=True),
+            ResidualBlock(96, 96, kernel_size, decoder=True),
+            Upsample(scale_factor=(kernel_size - 1)),
+        )
+
+        self.layer2 = Sequential(
+            ResidualBlock(96, 48, kernel_size, decoder=True),
+            ResidualBlock(48, 48, kernel_size, decoder=True),
+            ResidualBlock(48, 48, kernel_size, decoder=True),
+            Upsample(scale_factor=(kernel_size - 1)),
+        )
+
+        self.layer3 = Sequential(
+            ResidualBlock(48, 24, kernel_size, decoder=True),
+            ResidualBlock(24, 24, kernel_size, decoder=True),
+            ResidualBlock(24, 24, kernel_size, decoder=True),
+            Upsample(scale_factor=(kernel_size - 1)),
+        )
+
+        self.layer4 = Sequential(
+            ResidualBlock(24, 24, kernel_size, decoder=True),
+            ResidualBlock(24, 24, kernel_size, decoder=True),
+            ResidualBlock(24, 24, kernel_size, decoder=True),
+            Upsample(scale_factor=(kernel_size - 1)),
+        )
+
+        self.conv = ConvTranspose1d(
+            in_channels=24,
+            out_channels=channels_out,
+            kernel_size=kernel_size,
+            padding=same_padding,
+            bias=False,
+        )
+
+        self.bn = BatchNorm1d(12)
+        # TODO: is relu really appropriate here?
+        self.activation = ReLU()
+
+    def forward(self, x):
+        x = self.linear(x)
+        x = self.dropout(x)
+        x = self.unflatten(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.activation(x)
+
+        return x
+
+
+class Resnet1dVAE(L.LightningModule):
+
+    def __init__(
+        self,
+        kernel_size: int = 5,
+        latent_dim: int = 40,
+        dropout: float = 0.1,
+        lr: float = 1e-3,
+    ):
+        super(Resnet1dVAE, self).__init__()
+        self.encoder = Resnet1dEncoder(kernel_size, dropout, latent_dim)
+        self.decoder = Resnet1dDecoder(kernel_size, dropout, latent_dim)
+
+        self.mu = Linear(latent_dim, latent_dim)
+        self.sigma = Linear(latent_dim, latent_dim)
+
+        self.lr = lr
+
+    def _reparameterization(self, mean, var):
+        e = torch.randn_like(var)
+        return mean + (var * e)
+
+    def _loss_fn(self, x, x_hat, mean, var):
+        reproduction_loss = torch.nn.functional.mse_loss(x_hat, x, reduction="mean")
+        KLD = -0.5 * torch.sum(1 + var - mean.pow(2) - var.exp())
+
+        return reproduction_loss + KLD
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        m, s = self.mu(encoded), self.sigma(encoded)
+        z = self._reparameterization(m, s)
+        x_hat = self.decoder(z)
+
+        loss = self._loss_fn(x, x_hat, m, s)
+
+        return loss
+
+    def training_step(self, x):
+        loss = self.forward(x)
+        self.log("train_loss", loss)
+
+        return loss
+
+    def validation_step(self, x):
+        loss = self.forward(x)
+        self.log("val_loss", loss)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        return optimizer
+
+
 if __name__ == "__main__":
-    e = Resnet1dEncoder(0, 5, 0.1, 40)
+    e = Resnet1dEncoder(5, 0.1, 40).cuda()
+    d = Resnet1dDecoder(5, 0.1, 40).cuda()
 
     x = torch.randn((16, 12, 5000)).cuda()
-    summary(e.cuda(), x.shape)
+
+    summary(e, x.shape)
 
     encoded = e(x)
 
     print(encoded.shape)
+
+    # summary(d, encoded.shape)
+
+    decoded = d(encoded)
+
+    print(decoded.shape)
