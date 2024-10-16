68a775318d2e937bc8f734e2d68bd50774286167
isears <isaac.j.sears@gmail.com>
2024-10-16 07:57:55 -0400

    Make decoder symmetric with encoder

--- ptbxlae/modeling/singleCycleConv.py
+++ ptbxlae/modeling/singleCycleConv.py
@@ -27,12 +27,15 @@ class SingleCycleConvVAE(BaseVAE):
         latent_dim: int = 25,
         seq_len: int = 500,
         n_channels: int = 12,
+        # depth: int = 2,
+        # batchnorm: bool = False,
     ):
         super(SingleCycleConvVAE, self).__init__()
 
         self.lr = lr
         self.latent_dim = latent_dim
         same_padding = (kernel_size - 1) // 2
+        linear_size = (seq_len // 4) * (4 * n_channels)
 
         assert (
             seq_len % 4 == 0
@@ -62,23 +65,24 @@ class SingleCycleConvVAE(BaseVAE):
         self.fc_logvar = Linear((seq_len // 4) * (4 * n_channels), latent_dim)
 
         self.decoder = Sequential(
-            Linear(latent_dim, 1200),
-            Unflatten(dim=1, unflattened_size=(48, 25)),
-            Upsample(scale_factor=4),
+            Linear(latent_dim, (seq_len // 4) * (4 * n_channels)),
+            Unflatten(dim=1, unflattened_size=(48, seq_len // 4)),
             ConvTranspose1d(
                 in_channels=48,
                 out_channels=24,
                 kernel_size=kernel_size,
                 padding=same_padding,
+                stride=2,
+                output_padding=1,
             ),
-            BatchNorm1d(num_features=24),
             LeakyReLU(),
-            Upsample(scale_factor=5),
             ConvTranspose1d(
                 in_channels=24,
                 out_channels=12,
                 kernel_size=kernel_size,
                 padding=same_padding,
+                stride=2,
+                output_padding=1,
             ),
             # TODO: do we need these last two?
             # BatchNorm1d(num_features=12),
@@ -102,11 +106,11 @@ if __name__ == "__main__":
 
     mean, logvar = m.encode_mean_logvar(x)
 
-    print(e.shape)
-    print(z.shape)
-    print(mean.shape)
-    print(logvar.shape)
+    print(f"Encoder shape:\t {e.shape}")
+    print(f"Encoded shape:\t {z.shape}")
+    print(f"Mean shape:\t {mean.shape}")
+    print(f"Logvar shape:\t {logvar.shape}")
 
     d = m.decoder(z)
 
-    print(d.shape)
+    print(f"Decoded shape:\t {d.shape}")
