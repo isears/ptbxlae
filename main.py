b492fc69e5fd1bfbf61f23af1848c8055d5c82a8
isears <isaac.j.sears@gmail.com>
2024-12-27 06:27:12 -0500

    Subclass LightningCLI to allow for checkpoint loading in finetuning phase

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from ptbxlae.modeling import BaseVAE
 from lightning.pytorch.cli import LightningCLI
 from ptbxlae.dataprocessing.dataModules import BaseDM
+import torch
+
+
+class BaseModelLoadingCLI(LightningCLI):
+    def add_arguments_to_parser(self, parser):
+        parser.add_argument(
+            "--base_ckpt_path",
+            help="Path to load a pretrained checkpoint before .fit()",
+            type=str,
+        )
 
 
 def cli_main():
-    cli = LightningCLI(
+    cli = BaseModelLoadingCLI(
         BaseVAE,
         BaseDM,
         subclass_mode_data=True,
@@ -12,6 +22,14 @@ def cli_main():
         save_config_callback=None,
         run=False,
     )
+
+    # Need to load weights only because otherwise statedict of other objects e.g. early stopping checkpoint also get loaded
+    # https://github.com/Lightning-AI/pytorch-lightning/issues/15705
+
+    if cli.config.base_ckpt_path:
+        print(f"Loading weights from {cli.config.base_ckpt_path}")
+        weights = torch.load(cli.config.base_ckpt_path, map_location=cli.model.device)
+        cli.model.load_state_dict(weights["state_dict"])
 
     cli.trainer.fit(cli.model, cli.datamodule)
     cli.trainer.test(cli.model, cli.datamodule)
