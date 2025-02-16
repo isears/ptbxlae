a59be2c6ec9a9708c01346ae51b8627fc7e0e48c
isears <isaac.j.sears@gmail.com>
2025-02-16 12:47:55 +0000

    Remove redundant PTBXL clean DS

--- ptbxlae/dataprocessing/ptbxlDS.py
+++ ptbxlae/dataprocessing/ptbxlDS.py
@@ -24,40 +24,9 @@ class PtbxlDS(torch.utils.data.Dataset):
         Returns:
             None: None
         """
-        super().__init__()
+        super(PtbxlDS, self).__init__()
 
-        # If not filtered already, do filtering
-        if os.path.isfile(f"{root_folder}/ptbxl_database_filtered.csv"):
-            metadata = pd.read_csv(f"{root_folder}/ptbxl_database_filtered.csv")
-            metadata.patient_id = metadata.patient_id.astype(int)
-        # Filtering: only take one EKG per patient, and attempt to find a "clean" EKG for each patient
-        else:
-            metadata = pd.read_csv(f"{root_folder}/ptbxl_database.csv")
-            metadata.patient_id = metadata.patient_id.astype(int)
-
-            def get_first_clean(g):
-                clean_only = g[
-                    g[
-                        [
-                            "baseline_drift",
-                            "static_noise",
-                            "burst_noise",
-                            "electrodes_problems",
-                        ]
-                    ]
-                    .isna()
-                    .all(axis=1)
-                ]
-                if len(clean_only) > 0:
-                    return clean_only.iloc[0]
-                else:
-                    return None
-
-            metadata = metadata.groupby("patient_id").apply(get_first_clean)  # type: ignore
-            metadata = metadata.dropna(subset="ecg_id")
-
-            metadata.to_csv(f"{root_folder}/ptbxl_database_filtered.csv")
-
+        metadata = pd.read_csv(f"f{root_folder}/ptbxl_database.csv")
         metadata.ecg_id = metadata.ecg_id.astype(int)
         metadata.patient_id = metadata.patient_id.astype(int)
         self.metadata = metadata
@@ -118,36 +87,12 @@ class PtbxlDS(torch.utils.data.Dataset):
         """
         self.return_labels = return_labels
 
-class PtbxlCleanDS(PtbxlDS):
-    def _clean(self, sig_raw: np.ndarray, sigmeta: dict) -> np.ndarray:
-        sig_clean = np.apply_along_axis(
-            nk.ecg_clean,  # type: ignore
-            1,
-            sig_raw.transpose(),
-            sampling_rate=sigmeta["fs"],
-        )
 
-        return sig_clean
+class PtbxlMultilabeledDS(PtbxlDS):
+    def __getitem__(self, index):
+        sig, labels_dict = super().__getitem__(index)
+        return sig, torch.Tensor(list(labels_dict.values())).float()
 
-    def __getitem__(self, index: int):
-        this_meta = self.metadata.iloc[index]
-        ecg_id = this_meta["ecg_id"]
-        sig, sigmeta = load_single_ptbxl_record(
-            ecg_id, lowres=self.lowres, root_dir=self.root_folder
-        )
-
-        sig_clean = self._clean(sig, sigmeta)  # type: ignore
-
-        if self.return_labels:
-            return torch.Tensor(sig_clean).float(), self._get_labels(index)
-        else:
-            return torch.Tensor(sig_clean).float(), {}
-
-
-class PtbxlMultilabeledDS(PtbxlCleanDS):
-    def _get_labels(self, index):
-        labels_dict = super()._get_labels(index)
-        return torch.Tensor(list(labels_dict.values())).float()
 
 if __name__ == "__main__":
     ds = PtbxlDS(lowres=True, return_labels=True)
