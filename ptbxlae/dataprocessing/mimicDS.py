9b20af5f05354f9c57bbae87cd531383bc9a2031
isears <isaac.j.sears@gmail.com>
2025-02-18 14:25:06 +0000

    Start work on pulling additional mimic data from postgres

--- ptbxlae/dataprocessing/mimicDS.py
+++ ptbxlae/dataprocessing/mimicDS.py
@@ -4,6 +4,20 @@ import random
 import wfdb
 import neurokit2 as nk
 import numpy as np
+from sqlalchemy import (
+    create_engine,
+    MetaData,
+    Table,
+    Column,
+    Integer,
+    String,
+    DateTime,
+    select,
+    and_,
+)
+from sqlalchemy.ext.automap import automap_base
+
+import datetime
 
 
 class MimicDS(torch.utils.data.Dataset):
@@ -14,6 +28,31 @@ class MimicDS(torch.utils.data.Dataset):
         freq: int = 100,
     ):
         super().__init__()
+
+        # TODO: connection string params should be passed through args
+        self.db_engine = create_engine("postgresql+psycopg2://readonly@/mimiciv")
+
+        with self.db_engine.connect() as conn:
+            print(f"Successfully connected: {conn}")
+
+        self.db_metadata = MetaData()
+
+        for schema_name in [
+            "mimiciv_hosp",
+            "mimiciv_ed",
+            "mimiciv_icu",
+            "mimiciv_derived",
+        ]:
+            # NOTE: allows access to tables like self.db_metadata.tables['mimiciv_hosp.patients']
+            self.db_metadata.reflect(bind=self.db_engine, schema=schema_name)
+
+        # self.hosp_patients_table = Table(
+        #     "patients",
+        #     self.db_metadata,
+        #     autoload_with=self.db_engine,
+        #     schema="mimiciv_hosp",
+        # )
+
         self.root_folder = root_folder
         self.return_labels = return_labels
         self.record_list = (
@@ -26,6 +65,17 @@ class MimicDS(torch.utils.data.Dataset):
 
         random.seed(42)
 
+    def _get_patient_info(self, subject_id: int, date: datetime.date):
+        stmt = select(self.hosp_patients_table).where(
+            self.hosp_patients_table.columns.subject_id == subject_id
+        )
+
+        with self.db_engine.connect() as conn:
+            results = conn.execute(stmt).fetchall()
+            assert len(results) == 1  # subject_id should be unique in patients table
+
+        return results[0]
+
     def __len__(self):
         return len(self.record_list)
 
@@ -34,6 +84,10 @@ class MimicDS(torch.utils.data.Dataset):
         path = f"{self.root_folder}/{random.choice(possible_ecgs)}"
 
         sig, meta = wfdb.rdsamp(path)
+        subject_id = int(meta["comments"][0].split(":")[-1])
+        date = meta["base_date"]
+
+        patient_info = self._get_patient_info(subject_id, date)
 
         sig = sig.transpose()
 
@@ -77,7 +131,6 @@ class MimicDS(torch.utils.data.Dataset):
         sig_clean = np.apply_along_axis(
             nk.ecg_clean, 1, sig_resamp, sampling_rate=self.freq  # type: ignore
         )  # type: ignore
-
 
         return torch.Tensor(sig_clean).float(), {}
 
